4cbcd33d6a9c52370d6b7b4ca40958b0190a7d69
Andrey <arcevav@yandex.ru>
2023-04-27 16:40:27 +0500

    add code

--- Lesson2_task13/Program.cs
+++ Lesson2_task13/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Prompt(string msg)
+{
+
+System.Console.Write("msg ");
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число");
+
+int fnumber(int number)
+{
+while (number > 999)
+{
+number /= 10;
+}
+return number % 10;
+}
+
+bool check(int number)
+{
+if (number < 100)
+return false;
+else return true;
+}
+
+if (check(number) != true)
+System.Console.WriteLine("Третьей цифры нет");
+else
+System.Console.WriteLine("Третья цифра числа " + number + " является " + fnumber(number));
