6a9b846da723cfaeb9089493ecb04d0346571599
Andrey <arcevav@yandex.ru>
2023-04-27 14:15:48 +0500

    add work code

--- Lesson1_task2/Program.cs
+++ Lesson1_task2/Program.cs
@@ -1,2 +1,7 @@
-﻿Console.WriteLine("Введите второе число");
-
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.WriteLine("Большее число: " + a + ", меньшее число: " + b);
+else Console.WriteLine("Большее число: " + b + ", меньшее число: " + a);
