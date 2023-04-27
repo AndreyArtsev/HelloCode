63e9b14d400b5a6cb2aa0687a7c1cbf28b63b646
Andrey <arcevav@yandex.ru>
2023-04-27 15:41:38 +0500

    add code

--- Lesson1_task8/Program.cs
+++ Lesson1_task8/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i=1;i<=n;i++)
+{
+if (i % 2 == 0)
+    Console.WriteLine(i + ", ");
+}
