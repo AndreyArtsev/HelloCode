3e7749c4713749cc509ce3f57243dd2c4109df7b
Andrey <arcevav@yandex.ru>
2023-04-27 17:26:11 +0500

    add code

---
+++ Lesson2_task15/Program.cs
@@ -0,0 +1,15 @@
+﻿int Prompt(string msg)
+{
+
+System.Console.Write("msg ");
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число");
+string ss = "Это не день недели";
+string check(int number)
+{
+if (number >= 6 && number < 8) ss = "Это выходной";
+if (number >= 1 && number < 6) ss = "Это будний день";
+return ss;
+}
+System.Console.WriteLine(check(number));
