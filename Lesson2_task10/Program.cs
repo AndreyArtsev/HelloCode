ac4fba7b543f9e1e2cd71bcd437f905fa3b868b3
Andrey <arcevav@yandex.ru>
2023-04-27 16:30:23 +0500

    add code

--- Lesson2_task10/Program.cs
+++ Lesson2_task10/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Prompt(string msg)
+{
+    Console.Write("msg ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = Prompt("Введите трехзначное число");
+if (number >= 100 && number < 1000)
+{
+    int mid = number / 10 % 10;
+    System.Console.WriteLine("Вторая цифра числа " + number + " является " + mid);
+}
+else
+    Console.WriteLine("Это число не трехзначное");
