8e1bd140a9838910ee786d48aad4414bad3d8098
Andery Artsev <arcevav@yandex.ru>
2023-07-03 20:10:50 +0500

    add project file

--- Final_task/Program.cs
+++ Final_task/Program.cs
@@ -7,3 +7,29 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+Console.Write("Сколько элементов Вы хотите ввести: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] arrayStrings = new string[size];
+int length = 3;
+int position = 0;
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+    if (element.Length <= length)
+    {
+        arrayStrings[position] = element;
+        position++;
+    }
+}
+Console.WriteLine();
+PrintArray(arrayStrings);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
